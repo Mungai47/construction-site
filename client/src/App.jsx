93c2cd582e1cf01c07218b30e5bfeae0d55e51a0
Joseph <Josephmburumungai99@gmail.com>
2023-10-27 16:20:06 +0300

    Created pages and header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Blog from "./pages/Blog"
+import ContactUs from "./pages/ContactUs"
+import Services from "./pages/Services"
+import FAQ from "./pages/FAQ"
+import AboutUs from "./pages/AboutUs"
+import Header from "./components/Header"
+
 
 function App() {
   return (
-    <h1 className="text-red-500 font-extrabold text-2xl">App</h1>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/contact-us" element={<ContactUs />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/faq" element={<FAQ />} />
+      <Route path="/about-us" element={<AboutUs />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
