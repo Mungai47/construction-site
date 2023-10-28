ddec99da1a19be4865566f8bb5fa2181bb28cd97
Joseph <Josephmburumungai99@gmail.com>
2023-10-28 19:29:00 +0300

    customized the header and hero component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,39 +1,49 @@
-import { useState } from 'react';
-import { FaSearch } from 'react-icons/fa'
 import { TiThMenu } from 'react-icons/ti'
+import { Link } from 'react-router-dom';
 
 function Header() {
 
-    const tonglebutton =function(){
-        return (
-            <div></div>
-        )
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-50px";
+      }
+      prevScrollpos = currentScrollPos;
     }
 
   return (
-    <header className="p-4 shadow-lg">
-        <div className="w-5/6 mx-auto flex justify-between">        
-            <div className="flex items-center gap-2 shrink-0">
-                <img className="h-12 w-12" src="/logo.png" alt="logo" />
-                <h1 className="text-2xl sm:text-3xl font-extrabold">CornerStone</h1>
-            </div> 
+    <header className="p-4 shadow-lg navbar">
+        <div className="w-5/6 mx-auto flex justify-between items-center">        
+            <Link to="/">
+                <div className="flex items-center gap-2 shrink-0">
+                    <img className="h-12 w-12" src="/logo.png" alt="logo" />
+                    <h1 className="text-2xl sm:text-3xl font-extrabold">CornerStone</h1>
+                </div>
+            </Link> 
             <div className="lg:hidden">
                 {<TiThMenu />}
             </div>       
-            <div className="flex items-center ">
-                <ul className="flex items-center gap-3 lg:gap-10 text-lg sm:text-xl font-medium">
-                    <li className="hover:underline">Home</li>
-                    <li className="hover:underline">Services</li>
-                    <li className="hover:underline">About us</li>
-                    <li className="hover:underline">Contact us</li>
-                    <li className="hover:underline">Blog</li>
+            <div className=" hidden lg:inline">
+                <ul className="flex items-center gap-3 lg:gap-5 text-lg sm:text-xl font-medium">
+                    <Link to="/">
+                        <li className="hover:underline">Home</li>
+                    </Link>
+                    <Link to="/services">
+                        <li className="hover:underline">Services</li>
+                    </Link>
+                    <Link to="/about-us">
+                        <li className="hover:underline">About us</li>
+                    </Link>
+                    <Link to="/contact-us">
+                        <li className="hover:underline">Contact us</li>
+                    </Link>
+                    <Link to="/blog">
+                        <li className="hover:underline">Blog</li>
+                    </Link>
                 </ul>    
-            </div>
-            <div className="flex items-center">
-                <form className="flex items-center bg-[#d1d5db] rounded-xl p-2">
-                    <input type="text" placeholder="Search..." className="focus:outline-none bg-transparent"/>
-                    {<FaSearch />}
-                </form>
             </div>
         </div>
     </header>
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <div>
+        <div className="bg-[url('./image1.jpg')]">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Hero
